dc1777014c696f675cc1f730ce20df3fd722d344
apupneja <51889135+apupneja@users.noreply.github.com>
2021-03-19 19:59:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+// Sets the arrow in motion
+$('.round').click(function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $('.arrow').toggleClass('bounceAlpha');
+});
+
+// Google login stuff taken from the website
+function onSuccess(googleUser) {
+    console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+  }
+  function onFailure(error) {
+    console.log(error);
+  }
+  function renderButton() {
+    gapi.signin2.render('my-signin2', {
+      'scope': 'profile email',
+      'width': 240,
+      'height': 50,
+      'longtitle': true,
+      'theme': 'dark',
+      'onsuccess': onSuccess,
+      'onfailure': onFailure
+    });
+  }
