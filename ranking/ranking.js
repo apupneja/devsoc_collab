eec0ff931c1198aa4c6c9031620e418255fe61a4
apupneja <apupneja2002@gmail.com>
2021-03-26 11:17:37 +0530

    render table with inversion

--- ranking/ranking.js
+++ ranking/ranking.js
@@ -12,3 +12,26 @@ var swiper = new Swiper('.swiper-container', {
     },
     loop: true
 });
+
+$(window).on('load' , function () {
+  $("table").each(function () {
+      var $this = $(this);
+      var newrows = [];
+      $this.find("tr").each(function () {
+          var i = 0;
+          $(this).find("td,th").each(function () {
+              i++;
+              if (newrows[i] === undefined) {
+                  newrows[i] = $("<tr></tr>");
+              }
+              newrows[i].append($(this));
+          });
+      });
+      $this.find("tr").remove();
+      $.each(newrows, function () {
+          $this.append(this);
+      });
+  });
+
+  return false;
+});
